71e81019bc6829e6244eabc8a11a0f3e7c8e3a8c
Hrabi80 <Hrabi.ahmed80@gmail.com>
2025-02-20 16:10:16 +0100

    WIP: implementing hadd hall in frontend

--- backend/src/DTO/ConcertResponseDTO.php
+++ backend/src/DTO/ConcertResponseDTO.php
@@ -2,7 +2,8 @@
 
 namespace App\DTO;
 
-class ConcertResponseDto
+
+class ConcertResponseDTO
 {
     public int $id;
     public string $date;
--- frontend/src/app/components/concerts/concert-modal/concert-modal.component.ts
+++ frontend/src/app/components/concerts/concert-modal/concert-modal.component.ts
@@ -3,6 +3,10 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import Swal from 'sweetalert2';
 import { Concert } from '../../../interfaces/concert.interface';
 import { ConcertsService } from '../../../services/concerts.service';
+import { BandsService } from '../../../services/bands.service';
+import { HallsService } from '../../../services/halls.service';
+import { PartyHall } from '../../../interfaces/Party-hall.interface';
+import { Band } from '../../../interfaces/band.interface';
 @Component({
   selector: 'app-concert-modal',
   standalone: true,
@@ -11,53 +15,79 @@ import { ConcertsService } from '../../../services/concerts.service';
   styleUrl: './concert-modal.component.css'
 })
 export class ConcertModalComponent {
-      @Input() consertData: Concert | null = null;
-      @Output() close = new EventEmitter<void>(); 
-      @Output() bandSaved = new EventEmitter<void>(); 
-      hallForm!: FormGroup;
-       isEditMode = false;
+  @Input() consertData: Concert | null = null;
+  @Output() close = new EventEmitter<void>(); 
+  @Output() bandSaved = new EventEmitter<void>(); 
+  concertForm!: FormGroup;
+  isEditMode = false;
+  halls: PartyHall[] = [];
+  bands: Band[] = [];
+    constructor(private fb: FormBuilder, 
+              private service: ConcertsService,
+              private hallService: HallsService,
+              private bandService: BandsService
+    ) {}
 
-       constructor(private fb: FormBuilder, private service: ConcertsService) {}
+  ngOnInit() {
+      this.isEditMode = !!this.consertData;
+      this.concertForm = this.fb.group({
+        date: [this.consertData?.date || '', Validators.required],
+        party_hall_id: [this.consertData?.party_hall_id|| '', Validators.required],
+        band_ids: [this.consertData?.band_ids || '', Validators.required],
+      });
 
-       ngOnInit() {
-               this.isEditMode = !!this.consertData;
-               this.hallForm = this.fb.group({
-                 date: [this.consertData?.date || '', Validators.required],
-                 party_hall_id: [this.consertData?.party_hall_id|| '', Validators.required],
-                 band_ids: [this.consertData?.band_ids || '', Validators.required],
-               });
-             }
-           
-             submitForm() {
-               if (this.hallForm.invalid) {
-                 return;
-               }
-           
-               if (this.isEditMode && this.consertData) {
-                 this.service.updateConcert(this.consertData.id, this.hallForm.value).subscribe({
-                   next: () => {
-                     Swal.fire('Succès', 'Le groupe a été mis à jour !', 'success');
-                     this.bandSaved.emit();
-                     this.close.emit();
-                   },
-                   error: (err) => {
-                     Swal.fire('Erreur', 'Une erreur est survenue!', 'error');
-                     console.error(err);
-                   },
-                 });
-               } else {
-                 // Create new band
-                 this.service.createConcert(this.hallForm.value).subscribe({
-                   next: () => {
-                     Swal.fire('Succès', 'Le groupe est ajouté correctement!', 'success');
-                     this.bandSaved.emit();
-                     this.close.emit();
-                   },
-                   error: (err) => {
-                     Swal.fire('Erreur', 'Une erreur est survenue!', 'error');
-                     console.error(err);
-                   },
-                 });
-               }
-             }
+      this.fetchHalls();
+      this.fetchBands();
+    }
+  
+  submitForm() {
+    if (this.concertForm.invalid) {
+      return;
+    }
+      
+    if (this.isEditMode && this.consertData) {
+      this.service.updateConcert(this.consertData.id, this.concertForm.value).subscribe({
+        next: () => {
+          Swal.fire('Succès', 'Le groupe a été mis à jour !', 'success');
+          this.bandSaved.emit();
+          this.close.emit();
+        },
+        error: (err) => {
+          Swal.fire('Erreur', 'Une erreur est survenue!', 'error');
+          console.error(err);
+        },
+      });
+    } else {
+      // Create new band
+      this.service.createConcert(this.concertForm.value).subscribe({
+        next: () => {
+          Swal.fire('Succès', 'Le groupe est ajouté correctement!', 'success');
+          this.bandSaved.emit();
+          this.close.emit();
+        },
+        error: (err) => {
+          Swal.fire('Erreur', 'Une erreur est survenue!', 'error');
+          console.error(err);
+        },
+      });
+    }
+  }
+
+  fetchHalls() {
+    this.hallService.getHalls().subscribe({
+      next: (halls: PartyHall[]) => {
+        this.halls = halls;
+      },
+      error: (err) => console.error('Error fetching halls:', err)
+    });
+  }
+
+  fetchBands() {
+    this.bandService.getBands().subscribe({
+      next: (bands: Band[]) => {
+        this.bands = bands;
+      },
+      error: (err) => console.error('Error fetching bands:', err)
+    });
+  }
 }
